5a007baed8b090c1532cb1916e43d1e8b57657cc
ashtn15kimsey <ashtn15@gmail.com>
2021-04-20 13:16:35 -0700

    update

--- mainapp/mainapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    products = ["apple", "cherries", "pineapples", "strawberries"]
-    context = {
-        'products': products,
-    }
-    return render(request, "home.html", context)
